34544ae590f14f060353ea23d1dabb6264e681e9
yofreke <sparkyjoe94@aim.com>
2016-11-28 20:04:37 -0800

    serve should use options.devtool if present

--- src/multiConfGenerators/serve.js
+++ src/multiConfGenerators/serve.js
@@ -2,14 +2,14 @@ const webpack = require('webpack');
 
 const config = require('../config');
 
-module.exports = (conf) => {
+module.exports = (conf, options) => {
   // Add HMR plugin
   if (config.useHMR) {
     conf.plugin('webpackHMR', webpack.HotModuleReplacementPlugin);
   }
   // Add sourcemap rules and devServer configs
   conf.merge({
-    devtool: config.devtool,
+    devtool: options.devtool || config.devtool,
     devServer: {
       inline: true,
       hot: config.useHMR
